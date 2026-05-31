e9012cd7466402b410197a9834f4015fe515b064
MohsenAhmed2632002 <sphinx2632002@gmail.com>
2026-06-01 00:36:00 +0300

    New branch :Flutter First commit

--- apps/mobile/system/lib/main.dart
+++ apps/mobile/system/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
-
+//New branch :Flutter First commit
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
